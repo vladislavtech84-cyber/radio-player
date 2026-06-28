55a8cc06c723cfc4719eba1b46868e19145c6b68
vladislavtech84-cyber <vladislav.tech84@gmail.com>
2026-06-28 22:38:36 +0300

    Update record.py

--- record.py
+++ record.py
@@ -2,17 +2,15 @@ import urllib.request
 import time
 import sys
 
-# URL вашего радиопотока
-STREAM_URL = "https://radio.5-tv.ru/radio.mp3" 
+STREAM_URL = "https://5-tv.ru" 
 
-# Длительность записи: 1 час (3600 секунд)
-RECORD_DURATION = 60
+# Ровно 1 минута записи для теста
+RECORD_DURATION = 60 
 
 OUTPUT_FILE = f"record_{int(time.time())}.mp3"
 
 print(f"Старт записи потока: {STREAM_URL}")
 try:
-    # Добавляем User-Agent, чтобы сервер радио не блокировал робота
     req = urllib.request.Request(STREAM_URL, headers={'User-Agent': 'Mozilla/5.0'})
     with urllib.request.urlopen(req) as response, open(OUTPUT_FILE, 'wb') as out_file:
         start_time = time.time()
