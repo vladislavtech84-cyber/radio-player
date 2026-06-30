88af824e6ce906edae2af8048d083236acbc53cc
vladislavtech84-cyber <vladislav.tech84@gmail.com>
2026-06-30 21:33:06 +0300

    Update record.py

--- record.py
+++ record.py
@@ -4,10 +4,7 @@ import sys
 
 STREAM_URL = "https://radio.5-tv.ru/radio.mp3"
 RECORD_DURATION = 600 # 10 минут
-
-# Имя файла будет содержать время окончания записи (например, part_1930.mp3)
-end_time_struct = time.localtime(time.time() + RECORD_DURATION)
-filename = f"part_{time.strftime('%H%M', end_time_struct)}.mp3"
+filename = "part_1.mp3"
 
 print(f"Запуск записи в облаке. Файл: {filename}, Длительность: {RECORD_DURATION} сек.")
 
@@ -24,7 +21,7 @@ try:
             if not chunk:
                 break
             out_file.write(chunk)
-    print(f"Файл {filename} успешно записан!")
+    print("Файл успешно записан!")
 except Exception as e:
     print(f"Критическая ошибка: {e}")
     sys.exit(1)
